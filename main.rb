065653f6ccb50edfa849da666e4d627752185b7c
Jasper St. Pierre <jasperstpierre@hotmail.com>
2025-04-02 14:38:58 -0400

    feat: use server id env variable

--- main.rb
+++ main.rb
@@ -8,46 +8,38 @@ bot = Discordrb::Bot.new token: ENV["DISCORD_TOKEN"], intents: :unprivileged
 bot.run background: true
 
 # for now hardcoded to one channel because that's all I need it for
-channel = bot.servers.values.first.channels.find { |c| c.name == ENV["CHANNEL_NAME"] }
+channel = bot.servers[ENV["SERVER_ID"].to_i].channels.find { |c| c.name == ENV["CHANNEL_NAME"] }
 if channel.nil?
   puts "No channel found"
   exit
 end
 
-class Score
-  def self.file
-    File.open("scores", "a+") do |f|
-      yield f
+def win(player)
+  scores = {}
+  File.open("scores", "a+") do |f|
+    content = f.read
+    if content.length == 0
+      content = "#{ENV["WATCHED_1"]},#{ENV["WATCHED_2"]},0,0"
     end
-  end
+    content = content.split(",")
+    p1 = content[0]
+    p2 = content[1]
+    p1_score = content[2]
+    p2_score = content[3]
 
-  def self.win(player)
-    scores = {}
-    file do |f|
-      content = f.read
-      if content.length == 0
-        content = "#{ENV["WATCHED_1"]},#{ENV["WATCHED_2"]},0,0"
-      end
-      content = content.split(",")
-      p1 = content[0]
-      p2 = content[1]
-      p1_score = content[2]
-      p2_score = content[3]
-
-      if player == p1
-        p1_score = p1_score.to_i + 1
-      elsif player == p2
-        p2_score = p2_score.to_i + 1
-      end
-
-      f.truncate(0)
-      f.write("#{p1},#{p2},#{p1_score},#{p2_score}")
-      scores = { p1: { name: p1, score: p1_score }, p2: { name: p2, score: p2_score } }
+    if player == p1
+      p1_score = p1_score.to_i + 1
+    elsif player == p2
+      p2_score = p2_score.to_i + 1
     end
 
-    puts "WIN, #{player}"
-    return scores
+    f.truncate(0)
+    f.write("#{p1},#{p2},#{p1_score},#{p2_score}")
+    scores = { p1: { name: p1, score: p1_score }, p2: { name: p2, score: p2_score } }
   end
+
+  puts "WIN, #{player}"
+  return scores
 end
 
 EM.run do
@@ -62,12 +54,6 @@ EM.run do
 
   ws.on :message do |event|
     p [:message, event.data]
-    if event.data.start_with?("|challstr|")
-      challstr = event.data["|challstr|".length..]
-      response = Net::HTTP.post_form(URI("https://play.pokemonshowdown.com/api/login"), { challstr: challstr, name: ENV["NAME"], pass: ENV["PASS"] })
-      obj = JSON.parse(response.body[1..])
-      ws.send("|/trn #{ENV["NAME"]},0,#{obj["assertion"]}")
-    end
 
     if event.data.start_with?("|queryresponse|roomlist|")
       rooms = JSON.parse(event.data["|queryresponse|roomlist|".length..])
@@ -87,7 +73,7 @@ EM.run do
 
   ws.on :close do |event|
     ws = nil
-    puts [:close, event.data]
+    p [:close, event]
   end
 end
 
